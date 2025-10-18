300cf358c77f992ed50f1fb765f13273fc2895ac
virgantara <vinux.edu@gmail.com>
2025-10-18 19:22:35 +0700

    update

--- main.py
+++ main.py
@@ -15,6 +15,9 @@ from tqdm import tqdm
 from dataset import Fitzpatrick17kDataset
 import gdown
 import zipfile
+import argparse
+import wandb
+from modelku import get_model
 
 def set_seed(seed=42):
     random.seed(seed)
@@ -40,245 +43,237 @@ def download_dataset():
 	    zip_ref.extractall('dataset')  # Extract into 'dataset' folder
 
 
+def train(args):
 
-SEED = 42
-set_seed(SEED)
+	SEED = 42
+	set_seed(SEED)
 
-csv_path = 'fitzpatrick17k.csv'
-img_dir = 'dataset/data/finalfitz17k'
-os.makedirs(img_dir, exist_ok=True)
+	csv_path = 'fitzpatrick17k.csv'
+	img_dir = 'dataset/data/finalfitz17k'
+	os.makedirs(img_dir, exist_ok=True)
 
-train_transform = transforms.Compose([
-    transforms.Resize((256, 256)),               # Resize slightly larger
-    transforms.RandomResizedCrop(224),           # Random crop
-    transforms.RandomHorizontalFlip(p=0.5),      # Flip left/right
-    transforms.RandomRotation(degrees=15),       # Random rotation
-    transforms.ToTensor(),
-    transforms.Normalize((0.617, 0.477, 0.423), (0.232, 0.204, 0.207)),
-])
+	train_transform = transforms.Compose([
+	    transforms.Resize((256, 256)),               # Resize slightly larger
+	    transforms.RandomResizedCrop(224),           # Random crop
+	    transforms.RandomHorizontalFlip(p=0.5),      # Flip left/right
+	    transforms.RandomRotation(degrees=15),       # Random rotation
+	    transforms.ToTensor(),
+	    transforms.Normalize((0.617, 0.477, 0.423), (0.232, 0.204, 0.207)),
+	])
 
-test_transform = transforms.Compose([
-    transforms.Resize((224, 224)),               # Deterministic resize
-    transforms.ToTensor(),
-    transforms.Normalize((0.617, 0.477, 0.423), (0.232, 0.204, 0.207)),
-])
+	test_transform = transforms.Compose([
+	    transforms.Resize((224, 224)),               # Deterministic resize
+	    transforms.ToTensor(),
+	    transforms.Normalize((0.617, 0.477, 0.423), (0.232, 0.204, 0.207)),
+	])
 
-train_csv_path = "cv_splits/train_fold1.csv"
-test_csv_path = "cv_splits/test_fold1.csv"
+	fold = args.fold
+	train_csv_path = "cv_splits/train_fold"+fold+".csv"
+	test_csv_path = "cv_splits/test_fold"+fold+".csv"
 
-train_dataset = Fitzpatrick17kDataset(
-    csv_file=train_csv_path,
-    img_dir=img_dir,
-    transform=train_transform,  # No transform yet
-    img_ext=".jpg"
-)
+	train_dataset = Fitzpatrick17kDataset(
+	    csv_file=train_csv_path,
+	    img_dir=img_dir,
+	    transform=train_transform,  # No transform yet
+	    img_ext=".jpg"
+	)
 
-test_dataset = Fitzpatrick17kDataset(
-    csv_file=test_csv_path,
-    img_dir=img_dir,
-    transform=test_transform,  # No transform yet
-    img_ext=".jpg"
-)
+	test_dataset = Fitzpatrick17kDataset(
+	    csv_file=test_csv_path,
+	    img_dir=img_dir,
+	    transform=test_transform,  # No transform yet
+	    img_ext=".jpg"
+	)
 
-num_classes = train_dataset.num_classes
-print("Total samples:", len(train_dataset))
-print("Number of classes:", train_dataset.num_classes)
-# print("Classes:", dataset.classes)
-img, label = train_dataset[0]
-print("Image shape:", img.shape)
-print("Label index:", label, "->", train_dataset.idx2label[label])
+	num_classes = train_dataset.num_classes
+	# print("Total samples:", len(train_dataset))
+	# print("Number of classes:", train_dataset.num_classes)
+	# # print("Classes:", dataset.classes)
+	# img, label = train_dataset[0]
+	# print("Image shape:", img.shape)
+	# print("Label index:", label, "->", train_dataset.idx2label[label])
 
+	args.num_classes = num_classes
 
-# def calc_meanstd(dataset):
-#     loader = DataLoader(dataset, batch_size=64, shuffle=False, num_workers=4)
-    
-#     # Step 3: compute mean and std
-#     mean = torch.zeros(3)
-#     std = torch.zeros(3)
-#     num_samples = 0
-    
-#     for images, _ in tqdm(loader, desc="Computing mean & std"):
-#         batch_samples = images.size(0)  # number of images in batch
-#         num_samples += batch_samples
-    
-#         # (B, C, H, W) -> sum over B,H,W
-#         mean += images.sum(dim=[0, 2, 3])
-#         std += (images ** 2).sum(dim=[0, 2, 3])
-    
-#     # Final mean and std
-#     mean /= (num_samples * images.shape[2] * images.shape[3])
-#     std = torch.sqrt(std / (num_samples * images.shape[2] * images.shape[3]) - mean ** 2)
-    
-#     print("Mean:", mean.tolist())
-#     print("Std:", std.tolist())
-
-# # calc_meanstd(dataset)
-
-# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+	device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
-# BATCH_SIZE = 64
-# train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=2)
-# test_loader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=False, num_workers=2)
+	BATCH_SIZE = args.batch_size
+	train_loader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, num_workers=8)
+	test_loader = DataLoader(test_dataset, batch_size=args.test_batch_size, shuffle=False, num_workers=8)
 
-# weights = models.ResNet18_Weights.IMAGENET1K_V1
-# model = models.resnet18(weights=weights)
-# # model.fc = nn.Linear(model.fc.in_features, num_classes)
+	model = get_model(args)
 
-# in_features = model.fc.in_features
-# model.fc = nn.Sequential(
-#     nn.Dropout(p=0.2),          # simple but effective
-#     nn.Linear(in_features, num_classes)
-# )
+	model = model.to(device)
 
-# model = model.to(device)
+	total_params = sum(p.numel() for p in model.parameters())
+    trainable_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
 
-# for name, param in model.named_parameters():
-#     if 'layer4' in name or 'fc' in name:
-#         param.requires_grad = True
-#     else:
-#         param.requires_grad = False
-# # for param in model.parameters():
-# #     param.requires_grad = False
-    
-# # for param in model.fc.parameters():
-# #     param.requires_grad = True
+    print(f"Total parameters: {total_params / 1e6:.2f}M")
+    print(f"Trainable parameters: {trainable_params / 1e6:.2f}M")
 
-# criterion = nn.CrossEntropyLoss()
-# optimizer = torch.optim.Adam(model.parameters(), lr=1e-4,weight_decay=1e-4)
+	criterion = nn.CrossEntropyLoss()
+	optimizer = torch.optim.Adam(model.parameters(), lr=args.lr,weight_decay=1e-4)
 
 
-# NUM_EPOCHS = 100
+	NUM_EPOCHS = args.epochs
 
-# best_acc = 0.0
-# best_f1 = 0.0
-# best_model_path = f"resnet18.pth"
+	best_acc = 0.0
+	best_f1 = 0.0
+	best_model_path = f"resnet18.pth"
 
-# train_loss_list = []
-# test_loss_list = []
-# train_acc_list = []
-# test_acc_list = []
+	train_loss_list = []
+	test_loss_list = []
+	train_acc_list = []
+	test_acc_list = []
 
-# train_f1_list = []
-# test_f1_list = []
-# train_precision_list = []
-# test_precision_list = []
-# train_recall_list = []
-# test_recall_list = []
+	train_f1_list = []
+	test_f1_list = []
+	train_precision_list = []
+	test_precision_list = []
+	train_recall_list = []
+	test_recall_list = []
 
-# scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=30, gamma=0.1)
-# for epoch in range(NUM_EPOCHS): 
-#     model.train()
-#     correct_train, total_train = 0, 0
+	scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=30, gamma=0.1)
+	for epoch in range(NUM_EPOCHS): 
+	    model.train()
+	    correct_train, total_train = 0, 0
 
-#     running_train_loss = 0.0
+	    running_train_loss = 0.0
 
-#     all_train_preds = []
-#     all_train_labels = []
-#     for imgs, labels in tqdm(train_loader):
-#         imgs, labels = imgs.to(device), labels.to(device)
+	    all_train_preds = []
+	    all_train_labels = []
+	    for imgs, labels in tqdm(train_loader):
+	        imgs, labels = imgs.to(device), labels.to(device)
 
-#         optimizer.zero_grad()
-#         outputs = model(imgs)
-#         loss = criterion(outputs, labels)
-#         loss.backward()
-#         optimizer.step()
-        
-#         running_train_loss += loss.item() * imgs.size(0)
+	        optimizer.zero_grad()
+	        outputs = model(imgs)
+	        loss = criterion(outputs, labels)
+	        loss.backward()
+	        optimizer.step()
+	        
+	        running_train_loss += loss.item() * imgs.size(0)
 
-#         _, preds = torch.max(outputs, 1)
-#         correct_train += (preds == labels).sum().item()
-#         total_train += labels.size(0)
+	        _, preds = torch.max(outputs, 1)
+	        correct_train += (preds == labels).sum().item()
+	        total_train += labels.size(0)
 
-#         all_train_preds.extend(preds.cpu().numpy())
-#         all_train_labels.extend(labels.cpu().numpy())
+	        all_train_preds.extend(preds.cpu().numpy())
+	        all_train_labels.extend(labels.cpu().numpy())
 
-#     scheduler.step()
+	    scheduler.step()
 
-#     train_acc = 100 * correct_train / total_train
-#     train_acc_list.append(train_acc)
+	    train_acc = 100 * correct_train / total_train
+	    train_acc_list.append(train_acc)
 
-#     train_loss = running_train_loss / total_train
-#     train_loss_list.append(train_loss)
+	    train_loss = running_train_loss / total_train
+	    train_loss_list.append(train_loss)
 
-#     train_precision = precision_score(all_train_labels, all_train_preds, average='weighted', zero_division=0)
-#     train_recall = recall_score(all_train_labels, all_train_preds, average='weighted', zero_division=0)
-#     train_f1 = f1_score(all_train_labels, all_train_preds, average='weighted', zero_division=0)
+	    train_precision = precision_score(all_train_labels, all_train_preds, average='weighted', zero_division=0)
+	    train_recall = recall_score(all_train_labels, all_train_preds, average='weighted', zero_division=0)
+	    train_f1 = f1_score(all_train_labels, all_train_preds, average='weighted', zero_division=0)
 
-#     train_precision_list.append(train_precision)
-#     train_recall_list.append(train_recall)
-#     train_f1_list.append(train_f1)
-    
-#     model.eval()
-#     correct_test, total_test = 0, 0
-#     running_test_loss = 0.0
+	    train_precision_list.append(train_precision)
+	    train_recall_list.append(train_recall)
+	    train_f1_list.append(train_f1)
+	    
+	    model.eval()
+	    correct_test, total_test = 0, 0
+	    running_test_loss = 0.0
 
-#     all_test_preds = []
-#     all_test_labels = []
-#     with torch.no_grad():
-#         for imgs, labels in tqdm(test_loader):
-#             imgs, labels = imgs.to(device), labels.to(device)
-#             outputs = model(imgs)
-#             loss = criterion(outputs, labels)
-#             _, preds = torch.max(outputs, 1)
-#             correct_test += (preds == labels).sum().item()
+	    all_test_preds = []
+	    all_test_labels = []
+	    with torch.no_grad():
+	        for imgs, labels in tqdm(test_loader):
+	            imgs, labels = imgs.to(device), labels.to(device)
+	            outputs = model(imgs)
+	            loss = criterion(outputs, labels)
+	            _, preds = torch.max(outputs, 1)
+	            correct_test += (preds == labels).sum().item()
 
-#             running_test_loss += loss.item() * imgs.size(0)
-#             total_test += labels.size(0)
+	            running_test_loss += loss.item() * imgs.size(0)
+	            total_test += labels.size(0)
 
-#             all_test_preds.extend(preds.cpu().numpy())
-#             all_test_labels.extend(labels.cpu().numpy())
+	            all_test_preds.extend(preds.cpu().numpy())
+	            all_test_labels.extend(labels.cpu().numpy())
 
-#     test_acc = 100 * correct_test / total_test
-#     test_acc_list.append(test_acc)
+	    test_acc = 100 * correct_test / total_test
+	    test_acc_list.append(test_acc)
 
-#     test_precision = precision_score(all_test_labels, all_test_preds, average='weighted', zero_division=0)
-#     test_recall = recall_score(all_test_labels, all_test_preds, average='weighted', zero_division=0)
-#     test_f1 = f1_score(all_test_labels, all_test_preds, average='macro', zero_division=0)
+	    test_precision = precision_score(all_test_labels, all_test_preds, average='weighted', zero_division=0)
+	    test_recall = recall_score(all_test_labels, all_test_preds, average='weighted', zero_division=0)
+	    test_f1 = f1_score(all_test_labels, all_test_preds, average='macro', zero_division=0)
 
 
-#     test_loss = running_test_loss / total_test
-#     test_loss_list.append(test_loss)
+	    test_loss = running_test_loss / total_test
+	    test_loss_list.append(test_loss)
 
-#     test_precision_list.append(test_precision)
-#     test_recall_list.append(test_recall)
-#     test_f1_list.append(test_f1)
-#     if test_acc > best_acc:
-#         best_acc = test_acc
-#         torch.save(model.state_dict(), best_model_path)
+	    test_precision_list.append(test_precision)
+	    test_recall_list.append(test_recall)
+	    test_f1_list.append(test_f1)
+	    if test_acc > best_acc:
+	        best_acc = test_acc
+	        torch.save(model.state_dict(), best_model_path)
 
-#     if test_f1 > best_f1:
-#         best_f1 = test_f1
-#         torch.save(model.state_dict(), 'best_model_f1.pt')
+	    if test_f1 > best_f1:
+	        best_f1 = test_f1
+	        torch.save(model.state_dict(), 'best_model_f1.pt')
 
-#     print(f"Epoch [{epoch+1}/{NUM_EPOCHS}] - "
-#           f"Train Acc: {train_acc:.2f}% | Test Acc: {test_acc:.2f}% | "
-#           f"Train F1: {train_f1:.3f} | Test F1: {test_f1:.3f}")
+	    print(f"Epoch [{epoch+1}/{NUM_EPOCHS}] - "
+	          f"Train Acc: {train_acc:.2f}% | Test Acc: {test_acc:.2f}% | "
+	          f"Train F1: {train_f1:.3f} | Test F1: {test_f1:.3f}")
 
 
 
-# # Plot loss and accuracy curves
-# plt.figure(figsize=(10, 4))
+	# Plot loss and accuracy curves
+	plt.figure(figsize=(10, 4))
 
-# # ===== LOSS =====
-# plt.subplot(1, 2, 1)
-# plt.plot(train_loss_list, label='Train Loss')
-# plt.plot(test_loss_list, label='Test Loss')
-# plt.xlabel('Epoch')
-# plt.ylabel('Loss')
-# plt.title('Loss over Epochs')
-# plt.legend()
-# plt.grid(True)
+	# ===== LOSS =====
+	plt.subplot(1, 2, 1)
+	plt.plot(train_loss_list, label='Train Loss')
+	plt.plot(test_loss_list, label='Test Loss')
+	plt.xlabel('Epoch')
+	plt.ylabel('Loss')
+	plt.title('Loss over Epochs')
+	plt.legend()
+	plt.grid(True)
 
-# # ===== ACCURACY =====
-# plt.subplot(1, 2, 2)
-# plt.plot(train_acc_list, label='Train Accuracy')
-# plt.plot(test_acc_list, label='Test Accuracy')
-# plt.xlabel('Epoch')
-# plt.ylabel('Accuracy (%)')
-# plt.title('Accuracy over Epochs')
-# plt.legend()
-# plt.grid(True)
+	# ===== ACCURACY =====
+	plt.subplot(1, 2, 2)
+	plt.plot(train_acc_list, label='Train Accuracy')
+	plt.plot(test_acc_list, label='Test Accuracy')
+	plt.xlabel('Epoch')
+	plt.ylabel('Accuracy (%)')
+	plt.title('Accuracy over Epochs')
+	plt.legend()
+	plt.grid(True)
 
-# plt.tight_layout()
-# plt.show()
+	plt.tight_layout()
+	plt.show()
+
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser(description='Point Cloud Recognition')
+    parser.add_argument('--exp_name', type=str, default='exp', metavar='N',
+                        help='Name of the experiment')
+    parser.add_argument('--model_name', type=str, default='van', metavar='N',
+                        choices=['resnet18','van'])
+    parser.add_argument('--attn_type', type=str, default='SA', metavar='N',
+                        choices=['SA','LKA','LSKA','CBAM'])
+    parser.add_argument('--batch_size', type=int, default=32, metavar='batch_size',
+                        help='Size of batch)')
+    parser.add_argument('--test_batch_size', type=int, default=16, metavar='batch_size',
+                        help='Size of batch)')
+    parser.add_argument('--epochs', type=int, default=250, metavar='N',
+                        help='number of episode to train ')
+
+    parser.add_argument('--lr', type=float, default=0.0001, metavar='LR',
+                        help='learning rate (default: 0.001, 0.1 if using sgd)')
+    parser.add_argument('--momentum', type=float, default=0.9, metavar='M',
+                        help='SGD momentum (default: 0.9)')
+    parser.add_argument('--no_cuda', type=bool, default=False,
+                        help='enables CUDA training')
+    parser.add_argument('--seed', type=int, default=1, metavar='S',
+                        help='random seed (default: 1)')
+    args = parser.parse_args()
+	train(args)
---
+++ modelku.py
@@ -0,0 +1,123 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from torchvision import transforms, models
+from van_utils import OverlapPatchEmbed, Block, load_model_weights
+
+from timm.models.vision_transformer import _cfg
+from functools import partial
+
+
+class VAN(nn.Module):
+    def __init__(self, img_size=224, in_chans=3, num_classes=1000, embed_dims=[64, 128, 256, 512],
+                mlp_ratios=[4, 4, 4, 4], drop_rate=0., drop_path_rate=0., norm_layer=nn.LayerNorm,
+                 depths=[3, 4, 6, 3], num_stages=4, flag=False):
+        super().__init__()
+        if flag == False:
+            self.num_classes = num_classes
+        self.depths = depths
+        self.num_stages = num_stages
+
+        dpr = [x.item() for x in torch.linspace(0, drop_path_rate, sum(depths))]  # stochastic depth decay rule
+        cur = 0
+
+        for i in range(num_stages):
+            patch_embed = OverlapPatchEmbed(img_size=img_size if i == 0 else img_size // (2 ** (i + 1)),
+                                            patch_size=7 if i == 0 else 3,
+                                            stride=4 if i == 0 else 2,
+                                            in_chans=in_chans if i == 0 else embed_dims[i - 1],
+                                            embed_dim=embed_dims[i])
+
+            block = nn.ModuleList([Block(
+                dim=embed_dims[i], mlp_ratio=mlp_ratios[i], drop=drop_rate, drop_path=dpr[cur + j])
+                for j in range(depths[i])])
+            norm = norm_layer(embed_dims[i])
+            cur += depths[i]
+
+            setattr(self, f"patch_embed{i + 1}", patch_embed)
+            setattr(self, f"block{i + 1}", block)
+            setattr(self, f"norm{i + 1}", norm)
+
+        # classification head
+        self.head = nn.Linear(embed_dims[3], num_classes) if num_classes > 0 else nn.Identity()
+
+        self.apply(self._init_weights)
+
+    def _init_weights(self, m):
+        if isinstance(m, nn.Linear):
+            trunc_normal_(m.weight, std=.02)
+            if isinstance(m, nn.Linear) and m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+        elif isinstance(m, nn.LayerNorm):
+            nn.init.constant_(m.bias, 0)
+            nn.init.constant_(m.weight, 1.0)
+        elif isinstance(m, nn.Conv2d):
+            fan_out = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+            fan_out //= m.groups
+            m.weight.data.normal_(0, math.sqrt(2.0 / fan_out))
+            if m.bias is not None:
+                m.bias.data.zero_()
+
+    def freeze_patch_emb(self):
+        self.patch_embed1.requires_grad = False
+
+    @torch.jit.ignore
+    def no_weight_decay(self):
+        return {'pos_embed1', 'pos_embed2', 'pos_embed3', 'pos_embed4', 'cls_token'}  # has pos_embed may be better
+
+    def get_classifier(self):
+        return self.head
+
+    def reset_classifier(self, num_classes, global_pool=''):
+        self.num_classes = num_classes
+        self.head = nn.Linear(self.embed_dim, num_classes) if num_classes > 0 else nn.Identity()
+
+    def forward_features(self, x):
+        B = x.shape[0]
+
+        for i in range(self.num_stages):
+            patch_embed = getattr(self, f"patch_embed{i + 1}")
+            block = getattr(self, f"block{i + 1}")
+            norm = getattr(self, f"norm{i + 1}")
+            x, H, W = patch_embed(x)
+            for blk in block:
+                x = blk(x)
+            x = x.flatten(2).transpose(1, 2)
+            x = norm(x)
+            if i != self.num_stages - 1:
+                x = x.reshape(B, H, W, -1).permute(0, 3, 1, 2).contiguous()
+
+        return x.mean(dim=1)
+
+    def forward(self, x):
+        x = self.forward_features(x)
+        x = self.head(x)
+
+        return x
+
+def get_model(args):
+
+	if args.model_name == 'resnet18':
+		weights = models.ResNet18_Weights.IMAGENET1K_V1
+		model = models.resnet18(weights=weights)
+		# model.fc = nn.Linear(model.fc.in_features, num_classes)
+
+		in_features = model.fc.in_features
+		model.fc = nn.Sequential(
+		    nn.Dropout(p=0.2),          # simple but effective
+		    nn.Linear(in_features, num_classes)
+		)
+	elif args.model_name == 'van':
+		model = VAN(
+	        embed_dims=[32, 64, 160, 256], mlp_ratios=[8, 8, 4, 4],
+	        num_classes=args.num_classes,
+	        norm_layer=partial(nn.LayerNorm, eps=1e-6), depths=[3, 3, 5, 2],
+	        **kwargs)
+	    model.default_cfg = _cfg()
+	    if pretrained:
+	        model = load_model_weights(model, "van_b0", kwargs)
+
+	return model
+
+
