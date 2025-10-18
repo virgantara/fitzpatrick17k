746a1eda32329965e585290a6491e15588c7ab8d
virgantara <vinux.edu@gmail.com>
2025-10-18 17:56:59 +0700

    update

--- dataset.py
+++ dataset.py
@@ -1,6 +1,7 @@
 from torch.utils.data import Dataset
 from tqdm import tqdm
 from PIL import Image
+import pandas as pd
 
 class Fitzpatrick17kDataset(Dataset):
     def __init__(self, csv_file, img_dir, transform=None, target_transform=None, img_ext=".jpg"):
